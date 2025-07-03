0cbfa0ff027b253722f8281ef6df3e594851905d
jeevananthams3113 <jeevanantham.s2020ece@sece.ac.in>
2025-07-03 18:35:41 +0530

    day 2 task completed

---
+++ Day 2/Game1/game1.js
@@ -0,0 +1,60 @@
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const checkBtn = document.getElementById("checkBtn");
+const againBtn = document.querySelector(".againbtn");
+const inputValue = document.getElementById("inputValue");
+const guessedValue = document.getElementById("guessedValue");
+const guessedNumberText = document.getElementById("guessedNumber");
+const scoreText = document.getElementById("score");
+const highScoreText = document.getElementById("highScore");
+const message = document.getElementById("message");
+const container = document.querySelector(".container");
+
+console.log("Secret Number:", secretNumber);
+
+scoreText.textContent = `Score: ${score}`;
+highScoreText.textContent = `High Score: ${highScore}`;
+
+checkBtn.addEventListener("click", () => {
+  const guess = Number(inputValue.value);
+  console.log("User guessed:", guess);
+
+  if (!guess || guess < 1 || guess > 20) {
+    message.textContent = "Enter a number between 1 and 20";
+  } else if (guess === secretNumber) {
+    guessedValue.textContent = secretNumber;
+    message.textContent = "Correct Number!";
+    container.style.backgroundColor = "#60b347";
+
+    if (score > highScore) {
+      highScore = score;
+      highScoreText.textContent = `High Score: ${highScore}`;
+    }
+  } else {
+    if (score > 1) {
+      message.textContent = guess > secretNumber ? "Too high" : "Too low";
+      score--;
+    } else {
+      message.textContent = "You lost the game!";
+      score = 0;
+    }
+    scoreText.textContent = `Score: ${score}`;
+  }
+
+  guessedNumberText.textContent = `Guessed Number: ${guess}`;
+});
+
+againBtn.addEventListener("click", () => {
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  console.log("New Secret Number:", secretNumber);
+
+  score = 20;
+  inputValue.value = "";
+  guessedValue.textContent = "?";
+  guessedNumberText.textContent = "Guessed Number: ";
+  message.textContent = "Start guessing...";
+  scoreText.textContent = `Score: ${score}`;
+  container.style.backgroundColor = "black";
+});
---
+++ Day 2/Learn/script1.js
@@ -0,0 +1,20 @@
+//1.Tag selector
+const h=document.getElementsByTagName("p");
+console.log(h.a);
+
+//2.Id selector
+const i=document.getElementById("a");
+console.log(i);
+
+//3.Class selector
+const c=document.getElementsByClassName("c");
+console.log(c[0]);
+
+//4.Query selector
+const d=document.querySelector("p");
+console.log(d);
+
+//5.Query selectorAll
+const e = document.querySelectorAll("p");
+console.log(e[1]);
+
